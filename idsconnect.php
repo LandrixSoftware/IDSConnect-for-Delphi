e61694e0bca138e249e41e2e16376d35f054fad7
Sven Harazim <sven.harazim@landrix.de>
2026-08-18 15:40:29 +0200

    Callback-Endpunkt absichern, Zugangsdaten aus der Versionsverwaltung nehmen
    idsconnect.php hat die sid aus dem Request ungeprueft an den Dateipfad
    gehaengt. Damit liessen sich ueber "../" beliebige Dateien lesen und -
    abhaengig von den Webserver-Rechten - auch ueberschreiben.
    
    - sid muss jetzt eine UUID sein (striktes Regex), sonst 400
    - Groessenbegrenzung auf 4 MB, sonst 413
    - Ablage wird mit LOCK_EX geschrieben und auf 0600 gesetzt
    - Einmalabruf: nach der Uebergabe wird die Datei geloescht
    - abgelaufene Ablagen werden nach einer Stunde entfernt
    - Content-Type und Statuscodes werden gesetzt
    
    Ausserdem sind configuration.ini und Testdata/ jetzt in .gitignore -
    die Ini enthaelt Kundennummern, Benutzernamen und Klartextpasswoerter.

--- idsconnect.php
+++ idsconnect.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,37 +18,104 @@
  * under the License.
 */
 
-  if (!isset($_REQUEST["sid"]))
-    exit;
+// Callback-Endpunkt fuer die IDS-Connect-Rueckuebertragung.
+//
+// Der Unterordner "idsconnect" muss ausserhalb des oeffentlich lesbaren
+// Bereichs liegen bzw. die Berechtigung 700 haben.
+// Die Callback-URL muss per https erreichbar sein.
 
-  //Callback-Warenkorb vom Onlineshop im Ordner idsconnect unter der SID abspeichern
-  //der Unterordner idsconnect sollte die Berechtigung 700 haben, damit er oeffentlich nicht lesbar ist
-  //die Callback-URL muss per https erreichbar sein
-  if (isset($_REQUEST["warenkorb"]))
+const IDSCONNECT_DIR      = 'idsconnect';
+const IDSCONNECT_MAX_SIZE = 4 * 1024 * 1024; // max. Groesse eines Warenkorbs
+const IDSCONNECT_TTL      = 3600;            // Sekunden, danach gilt die Ablage als verfallen
+
+// Die sid stammt aus TIDSConnect.GetUuid und ist immer eine UUID ohne
+// geschweifte Klammern. Frueher wurde sie ungeprueft an den Dateipfad
+// gehaengt - damit liessen sich ueber "../" beliebige Dateien lesen und
+// ueberschreiben.
+function idsconnect_sid_path()
+{
+  if (!isset($_REQUEST['sid']))
+    return null;
+
+  $sid = $_REQUEST['sid'];
+  if (!is_string($sid))
+    return null;
+  if (!preg_match('/^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$/', $sid))
+    return null;
+
+  return IDSCONNECT_DIR . '/' . strtolower($sid);
+}
+
+// Abgelaufene Ablagen entfernen, damit keine Warenkoerbe unbegrenzt liegen bleiben
+function idsconnect_cleanup()
+{
+  if (!is_dir(IDSCONNECT_DIR))
+    return;
+  $limit = time() - IDSCONNECT_TTL;
+  foreach (glob(IDSCONNECT_DIR . '/*') as $file)
   {
-    $fp = fopen("idsconnect/".$_REQUEST["sid"], "w");
-    fputs($fp, $_REQUEST["warenkorb"]);
-    fclose($fp);
+    if (is_file($file) && filemtime($file) < $limit)
+      @unlink($file);
+  }
+}
+
+$path = idsconnect_sid_path();
+if ($path === null)
+{
+  http_response_code(400);
+  exit;
+}
+
+idsconnect_cleanup();
+
+if (isset($_REQUEST['warenkorb']))
+{
+  $warenkorb = $_REQUEST['warenkorb'];
+  if (!is_string($warenkorb) || strlen($warenkorb) > IDSCONNECT_MAX_SIZE)
+  {
+    http_response_code(413);
+    exit;
+  }
+
+  if (!is_dir(IDSCONNECT_DIR))
+    @mkdir(IDSCONNECT_DIR, 0700, true);
+
+  // LOCK_EX, damit ein paralleler Abruf keine halb geschriebene Datei liest
+  if (file_put_contents($path, $warenkorb, LOCK_EX) === false)
+  {
+    http_response_code(500);
+    exit;
+  }
+  @chmod($path, 0600);
+
+  header('Content-Type: text/html; charset=utf-8');
 ?>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>IDS-SCHNITTSTELLE</title>
 </head>
 <body>
-<h1>Schlie&szlig;en Sie nun den Browser und setzen Sie die Bearbeitung in der Software fort!</h1>    
+<h1>Schlie&szlig;en Sie nun den Browser und setzen Sie die Bearbeitung in der Software fort!</h1>
 </body>
 </html>
-<?
-  }
-  else
+<?php
+}
+else
+{
+  if (!is_file($path))
   {
-    if (file_exists("idsconnect/".$_REQUEST["sid"])) 
-    {
-      $datei = file("idsconnect/".$_REQUEST["sid"]);
-      foreach($datei as $meine_datei)
-      {
-        echo $meine_datei;
-      }
-    }
+    http_response_code(404);
+    exit;
   }
+
+  header('Content-Type: text/xml; charset=utf-8');
+  readfile($path);
+
+  // Einmalabruf: nach der Uebergabe an die Handwerkssoftware wird die
+  // Ablage geloescht, damit der Warenkorb nicht abrufbar liegen bleibt.
+  // Fuer die Mehrfachrueckgabe nach IDS 2.5.1 (multipleResult) muss diese
+  // Zeile entfallen und stattdessen auf die TTL oben vertraut werden.
+  @unlink($path);
+}
 ?>
